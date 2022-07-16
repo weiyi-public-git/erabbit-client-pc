0a8ac1ce09430e7ac77f6954df859853a5d0b880
weiyi <cwy76537191@gmail.com>
2022-07-16 22:06:20 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <!-- vue2.0需要根元素,vue3.0可以是代码片段 Fragment -->
+  <div class="container"></div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  // 使用vuex仓库
+  name: 'App',
+  components: {
+  },
+  setup () {
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
+</script>
+
+<style>
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import { createStore } from 'vuex'
 
+// vue2.0 创建仓库 new Vuex.Store({})
+// vue3.0 创建仓库 createStore({})
 export default createStore({
   state: {
   },

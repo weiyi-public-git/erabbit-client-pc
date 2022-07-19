60a6b3ef1395c95fc889601051ba2b826f2be490
weiyi <cwy76537191@gmail.com>
2022-07-19 23:24:52 +0800

    批量注册组件

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -4,21 +4,39 @@
 // vue3.0插件写法要素： 导出一个对象，有install函数，默认传入了App应用实例，在app基础之上扩展
 
 import defaultImg from '@/assets/images/200.png'
-import XtxSkeleton from './xtx-skeleton.vue'
-import XtxCarousel from './xtx-carousel.vue'
-import XtxMore from './xtx-more.vue'
-import XtxBread from './xtx-bread.vue'
-import XtxBreadItem from './xtx-bread-item.vue'
+// import XtxSkeleton from './xtx-skeleton.vue'
+// import XtxCarousel from './xtx-carousel.vue'
+// import XtxMore from './xtx-more.vue'
+// import XtxBread from './xtx-bread.vue'
+// import XtxBreadItem from './xtx-bread-item.vue'
+
+// 使用 require 提供的函数 context 加载某一个目录下的所有 .vue 后缀的文件。
+// 然后 context 函数会返回一个导入函数 importFn
+// 它又一个属性 keys() 获取所有的文件路径
+// 通过文件路径数组，通过遍历数组，再使用 importFn 根据路径导入组件对象
+// 遍历的同时进行全局注册即可
+
+// context(目录路径，是否加载子目录，加载文件的匹配正则)
+const importFn = require.context('./', false, /\.vue$/)
+console.log(importFn.keys())
 
 export default {
   install (app) {
     // 在app上进行扩展，app提供 component directive 函数
     // 如果要挂载原型 app.config.globalProperties.$http
-    app.component(XtxSkeleton.name, XtxSkeleton)
-    app.component(XtxCarousel.name, XtxCarousel)
-    app.component(XtxMore.name, XtxMore)
-    app.component(XtxBread.name, XtxBread)
-    app.component(XtxBreadItem.name, XtxBreadItem)
+    // app.component(XtxSkeleton.name, XtxSkeleton)
+    // app.component(XtxCarousel.name, XtxCarousel)
+    // app.component(XtxMore.name, XtxMore)
+    // app.component(XtxBread.name, XtxBread)
+    // app.component(XtxBreadItem.name, XtxBreadItem)
+    // 根据keys批量注册
+    importFn.keys().forEach(path => {
+      // 导入组件
+      const component = importFn(path).default
+      // 进行注册
+      app.component(component.name, component)
+    })
+
     // 定义指令
     defineDirective(app)
   }
--- src/components/library/xtx-bread-item.vue
+++ src/components/library/xtx-bread-item.vue
@@ -3,7 +3,6 @@
       <RouterLink v-if="to" :to="to"><slot /></RouterLink>
       <span v-else><slot /></span>
     </div>
-    <i class="iconfont icon-angle-right"></i>
 </template>
 
 <script>

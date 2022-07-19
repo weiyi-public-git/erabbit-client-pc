b93c4bcce9ed2e25030cc0c5e7fe161b013ffef3
weiyi <cwy76537191@gmail.com>
2022-07-19 08:49:59 +0800

    首页轮播图的基础布局

---
+++ src/views/home/components/home-banner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home-banner">
+    <XtxCarousel />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeBanner'
+}
+</script>
+
+<style lang="less" scoped>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+}
+// 覆盖样式
+.xtx-carousel {
+  ::v-deep .carousel-btn.prev {
+    left: 270px;
+  }
+  ::v-deep .carousel-indicator {
+    padding-left: 250px;
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,24 +1,28 @@
 <template>
-  <div class="container">
-    <!-- 首页入口 -->
-    <div class="home-entry">
+  <!-- 首页入口 -->
+  <div class="home-entry">
+    <div class="container">
       <!-- 左侧分类 -->
       <HomeCategory />
       <!-- 轮播图 -->
+      <HomeBanner />
     </div>
   </div>
+  <!-- 新鲜好物 -->
+  <!-- 人气推荐 -->
 </template>
 
 <script>
 import HomeCategory from './components/home-category'
+import HomeBanner from './components/home-banner'
 export default {
   name: 'HomePage',
-  components: { HomeCategory }
+  components: { HomeCategory, HomeBanner }
 }
 </script>
 
 <style lang="less" scoped>
 .container {
-  color:@xtxColor
+  color: @xtxColor;
 }
 </style>>
